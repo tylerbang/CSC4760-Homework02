f4e6b85832be860a76cdbce35fede5a0958c6df1
tylerbang <tylerbang@protonmail.com>
2024-03-27 10:32:10 -0500

    Problem 3 complete, not yet tested. Problem 2 is in progress, will come back to it later.

---
+++ example/homework/02/init.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+#include <mpi.h>
+#include <iostream>
+#include <assert.h>
+
+void zero_domain(char *domain, int M, int N);
+void print_domain(char *domain, int M, int N);
+void update_domain(char *new_domain, char *old_domain, int M, int N);
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Finalize();
+    return 0;
+}
--- example/homework/03/init.cpp
+++ example/homework/03/init.cpp
@@ -5,14 +5,34 @@ int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
   // Make View of length n > 1000 and add values
-  
-  // create two additional views of same size and datatype
- 
-  // deep_copy
-  
-  // user copy
+  int n = 2500;
+  Kokkos::View<double*> a("a", n);
+  Kokkos::parallel_for(n, KOKKOS_LAMBDA(const int i) {
+    a(i) = i * i * 3.14;
+  });
+  Kokkos::fence();
 
+  // create two additional views of same size and datatype
+  Kokkos::View<double*> b("b", n);
+  Kokkos::View<double*> c("c", n);
+
+  // deep_copy
+  Kokkos::Timer timer;
+  Kokkos::deep_copy(b, a);
+  Kokkos::fence();
+  double time_deep_copy = timer.seconds();
+
+  // user copy
+  timer.reset();
+  Kokkos::parallel_for(n, KOKKOS_LAMBDA(const int i) {
+    c(i) = a(i);
+  });
+  Kokkos::fence();
+  double time_user_copy = timer.seconds();
+  
   // Output times 
+  cout << "Time for deep_copy: " << time_deep_copy << endl;
+  cout << "Time for user copy: " << time_user_copy << endl;
   }
   Kokkos::finalize();
 }
